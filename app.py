1f7e4569917caedfa9e108381b7e956521e81fc4
rahulchauhan22 <rahul7840041723@gmail.com>
2025-05-01 13:30:51 +0530

    upadate app.run

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
@@ -34,5 +35,6 @@ def recommend():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
